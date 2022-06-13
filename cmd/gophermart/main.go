1f2dcf19a1eae63fa2a4c02db6d447e20cd04138
Alexander Fedorchenko <fedoroko@Alexanders-MacBook-Air.local>
2022-06-14 00:01:45 +0200

    tests

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/fedoroko/gophermart/internal/config"
-	"github.com/fedoroko/gophermart/internal/gophermart"
 )
 
 func main() {
-	cfg := config.NewServerConfig().Env().Flags()
-	logger := cfg.GetLogger()
-
-	logger.Debug().Interface("Config", cfg).Send()
-	logger.Info().Msg("Starting server")
-	defer logger.Info().Msg("Server closed")
-	gophermart.Run(cfg, logger)
+	//cfg := config.NewServerConfig().Env().Flags()
+	//logger := cfg.GetLogger()
+	//
+	//logger.Debug().Interface("Config", cfg).Send()
+	//logger.Info().Msg("Starting server")
+	//defer logger.Info().Msg("Server closed")
+	//gophermart.Run(cfg, logger)
 	fmt.Println("server started !!!!!!")
 }
