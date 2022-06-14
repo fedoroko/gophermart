35bc570eeaf06207dad1bf4e97e306d22d45e10a
Alexander Fedorchenko <fedoroko@Alexanders-MacBook-Air.local>
2022-06-14 05:41:39 +0200

    chi test

--- internal/gophermart/gophermart.go
+++ internal/gophermart/gophermart.go
@@ -2,16 +2,16 @@ package gophermart
 
 import (
 	"github.com/fedoroko/gophermart/internal/accrual"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	"github.com/gin-gonic/gin"
-
 	"github.com/fedoroko/gophermart/internal/config"
 	"github.com/fedoroko/gophermart/internal/handlers"
-	"github.com/fedoroko/gophermart/internal/middlewares"
 	"github.com/fedoroko/gophermart/internal/storage"
 )
 
@@ -31,19 +31,18 @@ func Run(cfg *config.ServerConfig, logger *config.Logger) {
 	}()
 
 	r := router(db, q, logger)
-	r.Run(cfg.Address)
-	//server := &http.Server{
-	//	Addr:    "localhost:8080",
-	//	Handler: r,
-	//}
-	//
-	//defer server.Close()
-	//go func() {
-	//	logger.Info().Msg("Server started at " + cfg.Address)
-	//	if err = server.ListenAndServe(); err != nil {
-	//		logger.Error().Stack().Err(err).Send()
-	//	}
-	//}()
+	server := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: r,
+	}
+
+	defer server.Close()
+	go func() {
+		logger.Info().Msg("Server started at " + cfg.Address)
+		if err = server.ListenAndServe(); err != nil {
+			logger.Error().Stack().Err(err).Send()
+		}
+	}()
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig,
@@ -54,39 +53,31 @@ func Run(cfg *config.ServerConfig, logger *config.Logger) {
 	<-sig
 }
 
-func router(db storage.Repo, q accrual.Queue, logger *config.Logger) *gin.Engine {
-	r := gin.New()
-	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
-	r.Use(middlewares.InstanceID(1))
-	r.Use(middlewares.RateLimit())
+func router(db storage.Repo, q accrual.Queue, logger *config.Logger) chi.Router {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Compress(5))
 
 	h := handlers.Handler(db, q, logger, time.Second*30)
-	r.GET("/ping", h.Ping)
-	authBasic := middlewares.AuthBasic(db, logger)
-	authWithBalance := middlewares.AuthWithBalance(db, logger)
 
-	api := r.Group("api/user")
-	{
-		api.POST("/login", h.LoginFunc)
-		api.POST("/register", h.RegisterFunc)
-		api.POST("/logout", authBasic, h.LogoutFunc)
-
-		orders := api.Group("/orders")
-		orders.Use(authBasic)
-		{
-			orders.POST("/", h.OrderFunc)
-			orders.GET("/", h.OrdersFunc)
-		}
-
-		balance := api.Group("/balance")
-		balance.Use(authWithBalance)
-		{
-			balance.GET("/", h.BalanceFunc)
-			balance.POST("/withdraw", h.WithdrawFunc)
-			balance.GET("/withdrawals", h.WithdrawalsFunc)
-		}
-	}
+	r.Route("/api/user", func(r chi.Router) {
+		r.Post("/login", h.LoginFunc)
+		r.Post("/register", h.RegisterFunc)
+		r.Post("/logout", h.LoginFunc)
+		//r.Route("orders/", func(r chi.Router) {
+		//	r.Post("/", h.OrderFunc)
+		//	r.Get("/", h.OrdersFunc)
+		//})
+		//r.Route("/balance", func(r chi.Router) {
+		//	r.Get("/", h.BalanceFunc)
+		//	r.Post("/withdrawal", h.WithdrawFunc)
+		//	r.Get("/withdrawals", h.WithdrawalsFunc)
+		//})
+	})
 
 	return r
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,15 +4,11 @@ import (
 	"context"
 	"errors"
 	"github.com/fedoroko/gophermart/internal/accrual"
-	"github.com/fedoroko/gophermart/internal/withdrawals"
 	"net/http"
 	"time"
 
-	"github.com/gin-gonic/gin"
-
 	"github.com/fedoroko/gophermart/internal/config"
 	"github.com/fedoroko/gophermart/internal/controllers"
-	"github.com/fedoroko/gophermart/internal/orders"
 	"github.com/fedoroko/gophermart/internal/storage"
 	"github.com/fedoroko/gophermart/internal/users"
 )
@@ -33,202 +29,210 @@ func Handler(r storage.Repo, q accrual.Queue, logger *config.Logger, timeout tim
 	}
 }
 
-func (h *handler) LoginFunc(c *gin.Context) {
-	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
+func (h *handler) LoginFunc(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(), h.timeout)
 	defer cancel()
 
-	token, err := h.ctrl.Login(ctx, c.Request.Body)
+	defer r.Body.Close()
+
+	token, err := h.ctrl.Login(ctx, r.Body)
 	if err != nil {
 		switch {
 		case errors.As(err, &users.WrongPairError):
-			c.AbortWithStatusJSON(http.StatusUnauthorized, err.Error())
+			w.WriteHeader(http.StatusUnauthorized)
+			//c.AbortWithStatusJSON(http.StatusUnauthorized, err.Error())
 		case errors.As(err, &users.BadFormatError):
-			c.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			//c.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
 		default:
 			h.logger.Error().Stack().Err(err).Send()
-			c.AbortWithStatus(http.StatusInternalServerError)
+			w.WriteHeader(http.StatusInternalServerError)
+			//c.AbortWithStatus(http.StatusInternalServerError)
 		}
 		return
 	}
-	c.Set("Authorization", token)
-	c.JSON(http.StatusOK, token)
+
+	w.Header().Set("Authorization", token)
 }
 
-func (h *handler) RegisterFunc(c *gin.Context) {
-	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
+func (h *handler) RegisterFunc(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(), h.timeout)
 	defer cancel()
 
-	token, err := h.ctrl.Register(ctx, c.Request.Body)
+	defer r.Body.Close()
+	token, err := h.ctrl.Register(ctx, r.Body)
 	if err != nil {
 		switch {
 		case errors.As(err, &users.AlreadyExistsError):
-			c.AbortWithStatusJSON(http.StatusConflict, err.Error())
+			w.WriteHeader(http.StatusConflict)
+			//c.AbortWithStatusJSON(http.StatusConflict, err.Error())
 		case errors.As(err, &users.BadFormatError):
-			c.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			//c.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
 		default:
 			h.logger.Error().Stack().Err(err).Send()
-			c.AbortWithStatus(http.StatusInternalServerError)
+			w.WriteHeader(http.StatusInternalServerError)
+			//c.AbortWithStatus(http.StatusInternalServerError)
 		}
 		return
 	}
 
-	c.Set("Authorization", token)
-	c.JSON(http.StatusOK, token)
+	w.Header().Set("Authorization", token)
 }
 
-func (h *handler) LogoutFunc(c *gin.Context) {
-	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
-	defer cancel()
-
-	var s *users.Session
-	if err := h.ctrl.Logout(ctx, s); err != nil {
-		h.logger.Error().Stack().Err(err).Send()
-		// TO-DO
-		c.AbortWithStatus(http.StatusBadRequest)
-	}
-
-	c.Redirect(http.StatusOK, "/")
-}
-
-func (h *handler) OrderFunc(c *gin.Context) {
-	if ok := h.ValidateContentType(c, "text/plain"); !ok {
-		return
-	}
-
-	s, ok := h.GetSessionHelper(c)
-	if !ok {
-		return
-	}
-
-	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
-	defer cancel()
-
-	if err := h.ctrl.Order(ctx, s.User(), c.Request.Body); err != nil {
-		switch {
-		case errors.As(err, &orders.InvalidRequestError):
-			c.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
-		case errors.As(err, &orders.InvalidNumberError):
-			c.AbortWithStatusJSON(http.StatusUnprocessableEntity, err.Error())
-		case errors.As(err, &orders.AlreadyExistsError):
-			c.AbortWithStatusJSON(http.StatusOK, err.Error())
-		case errors.As(err, &orders.BelongsToAnotherError):
-			c.AbortWithStatusJSON(http.StatusConflict, err.Error())
-		default:
-			h.logger.Error().Stack().Err(err).Send()
-			c.AbortWithStatus(http.StatusInternalServerError)
-		}
-		return
-	}
-
-	c.Status(http.StatusAccepted)
-
-}
-
-func (h *handler) OrdersFunc(c *gin.Context) {
-	s, ok := h.GetSessionHelper(c)
-	if !ok {
-		return
-	}
-
-	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
-	defer cancel()
-	data, err := h.ctrl.Orders(ctx, s.User())
-	if err != nil {
-		switch {
-		case errors.As(err, &orders.NoItemsError):
-			c.AbortWithStatus(http.StatusNoContent)
-		default:
-			h.logger.Error().Stack().Err(err).Send()
-			c.AbortWithStatus(http.StatusInternalServerError)
-		}
-		return
-	}
-
-	c.JSON(http.StatusOK, data)
-}
-
-func (h *handler) BalanceFunc(c *gin.Context) {
-	s, ok := h.GetSessionHelper(c)
-	if !ok {
-		return
-	}
-
-	c.JSON(http.StatusOK, s.User())
-}
-
-func (h *handler) WithdrawFunc(c *gin.Context) {
-	if ok := h.ValidateContentType(c, "application/json"); !ok {
-		return
-	}
-
-	s, ok := h.GetSessionHelper(c)
-	if !ok {
-		return
-	}
-
-	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
-	defer cancel()
-
-	if err := h.ctrl.Withdraw(ctx, s.User(), c.Request.Body); err != nil {
-		switch {
-		case errors.As(err, &withdrawals.InvalidOrderError):
-			c.AbortWithStatusJSON(http.StatusUnprocessableEntity, err.Error())
-		case errors.As(err, &withdrawals.NotEnoughBalanceError):
-			c.AbortWithStatusJSON(http.StatusPaymentRequired, err.Error())
-		default:
-			h.logger.Error().Stack().Err(err).Send()
-			c.AbortWithStatus(http.StatusInternalServerError)
-		}
-		return
-	}
-
-	c.Status(http.StatusOK)
-}
-
-func (h *handler) WithdrawalsFunc(c *gin.Context) {
-	s, ok := h.GetSessionHelper(c)
-	if !ok {
-		return
-	}
-
-	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
-	defer cancel()
-	data, err := h.ctrl.Withdrawals(ctx, s.User())
-	if err != nil {
-		switch {
-		case errors.As(err, &withdrawals.NoRecordsError):
-			c.AbortWithStatus(http.StatusNoContent)
-		default:
-			h.logger.Error().Stack().Err(err).Send()
-			c.AbortWithStatus(http.StatusInternalServerError)
-		}
-		return
-	}
-
-	c.JSON(http.StatusOK, data)
-}
-
-func (h *handler) Ping(c *gin.Context) {
-	c.JSON(http.StatusOK, "pong")
-}
-
-func (h *handler) GetSessionHelper(c *gin.Context) (*users.Session, bool) {
-	data, ok := c.Get("session")
-	if data == nil || !ok {
-		h.logger.Error().Msg("middleware passed but session not found")
-		c.AbortWithStatus(http.StatusInternalServerError)
-		return nil, false
-	}
-
-	return data.(*users.Session), true
-}
-
-func (h *handler) ValidateContentType(c *gin.Context, t string) bool {
-	ct := c.GetHeader("Content-type")
-	if ct != t {
-		c.AbortWithStatus(http.StatusBadRequest)
-		return false
-	}
-
-	return true
-}
+//func (h *handler) LogoutFunc(c *gin.Context) {
+//	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
+//	defer cancel()
+//
+//	var s *users.Session
+//	if err := h.ctrl.Logout(ctx, s); err != nil {
+//		h.logger.Error().Stack().Err(err).Send()
+//		// TO-DO
+//		c.AbortWithStatus(http.StatusBadRequest)
+//	}
+//
+//	c.Redirect(http.StatusOK, "/")
+//}
+//
+//func (h *handler) OrderFunc(c *gin.Context) {
+//	if ok := h.ValidateContentType(c, "text/plain"); !ok {
+//		return
+//	}
+//
+//	s, ok := h.GetSessionHelper(c)
+//	if !ok {
+//		return
+//	}
+//
+//	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
+//	defer cancel()
+//
+//	if err := h.ctrl.Order(ctx, s.User(), c.Request.Body); err != nil {
+//		switch {
+//		case errors.As(err, &orders.InvalidRequestError):
+//			c.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
+//		case errors.As(err, &orders.InvalidNumberError):
+//			c.AbortWithStatusJSON(http.StatusUnprocessableEntity, err.Error())
+//		case errors.As(err, &orders.AlreadyExistsError):
+//			c.AbortWithStatusJSON(http.StatusOK, err.Error())
+//		case errors.As(err, &orders.BelongsToAnotherError):
+//			c.AbortWithStatusJSON(http.StatusConflict, err.Error())
+//		default:
+//			h.logger.Error().Stack().Err(err).Send()
+//			c.AbortWithStatus(http.StatusInternalServerError)
+//		}
+//		return
+//	}
+//
+//	c.Status(http.StatusAccepted)
+//
+//}
+//
+//func (h *handler) OrdersFunc(c *gin.Context) {
+//	s, ok := h.GetSessionHelper(c)
+//	if !ok {
+//		return
+//	}
+//
+//	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
+//	defer cancel()
+//	data, err := h.ctrl.Orders(ctx, s.User())
+//	if err != nil {
+//		switch {
+//		case errors.As(err, &orders.NoItemsError):
+//			c.AbortWithStatus(http.StatusNoContent)
+//		default:
+//			h.logger.Error().Stack().Err(err).Send()
+//			c.AbortWithStatus(http.StatusInternalServerError)
+//		}
+//		return
+//	}
+//
+//	c.JSON(http.StatusOK, data)
+//}
+//
+//func (h *handler) BalanceFunc(c *gin.Context) {
+//	s, ok := h.GetSessionHelper(c)
+//	if !ok {
+//		return
+//	}
+//
+//	c.JSON(http.StatusOK, s.User())
+//}
+//
+//func (h *handler) WithdrawFunc(c *gin.Context) {
+//	if ok := h.ValidateContentType(c, "application/json"); !ok {
+//		return
+//	}
+//
+//	s, ok := h.GetSessionHelper(c)
+//	if !ok {
+//		return
+//	}
+//
+//	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
+//	defer cancel()
+//
+//	if err := h.ctrl.Withdraw(ctx, s.User(), c.Request.Body); err != nil {
+//		switch {
+//		case errors.As(err, &withdrawals.InvalidOrderError):
+//			c.AbortWithStatusJSON(http.StatusUnprocessableEntity, err.Error())
+//		case errors.As(err, &withdrawals.NotEnoughBalanceError):
+//			c.AbortWithStatusJSON(http.StatusPaymentRequired, err.Error())
+//		default:
+//			h.logger.Error().Stack().Err(err).Send()
+//			c.AbortWithStatus(http.StatusInternalServerError)
+//		}
+//		return
+//	}
+//
+//	c.Status(http.StatusOK)
+//}
+//
+//func (h *handler) WithdrawalsFunc(c *gin.Context) {
+//	s, ok := h.GetSessionHelper(c)
+//	if !ok {
+//		return
+//	}
+//
+//	ctx, cancel := context.WithTimeout(c.Request.Context(), h.timeout)
+//	defer cancel()
+//	data, err := h.ctrl.Withdrawals(ctx, s.User())
+//	if err != nil {
+//		switch {
+//		case errors.As(err, &withdrawals.NoRecordsError):
+//			c.AbortWithStatus(http.StatusNoContent)
+//		default:
+//			h.logger.Error().Stack().Err(err).Send()
+//			c.AbortWithStatus(http.StatusInternalServerError)
+//		}
+//		return
+//	}
+//
+//	c.JSON(http.StatusOK, data)
+//}
+//
+//func (h *handler) Ping(c *gin.Context) {
+//	c.JSON(http.StatusOK, "pong")
+//}
+//
+//func (h *handler) GetSessionHelper(c *gin.Context) (*users.Session, bool) {
+//	data, ok := c.Get("session")
+//	if data == nil || !ok {
+//		h.logger.Error().Msg("middleware passed but session not found")
+//		c.AbortWithStatus(http.StatusInternalServerError)
+//		return nil, false
+//	}
+//
+//	return data.(*users.Session), true
+//}
+//
+//func (h *handler) ValidateContentType(c *gin.Context, t string) bool {
+//	ct := c.GetHeader("Content-type")
+//	if ct != t {
+//		c.AbortWithStatus(http.StatusBadRequest)
+//		return false
+//	}
+//
+//	return true
+//}
